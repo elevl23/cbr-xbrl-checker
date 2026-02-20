0ee622cf2b9d545356b120cd86c33f6bb26b07ea
elevl23 <Elena.PETROCHENKO@raiffeisen.ru>
2026-02-20 16:00:23 +0300

    Update dify-file.js

--- pages/api/dify-file.js
+++ pages/api/dify-file.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
       },
       {
         headers: {
-          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+          Authorization: `Bearer ${process.env.GH_TOKEN}`,
           'Content-Type': 'application/json'
         }
       }
